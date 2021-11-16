e2b4a016a041d9e22a52c2f23e0b5f01d2a6f00d
MelchiorSchuh <melchior.schuh-senlis@geode-solutions.com>
2021-11-16 12:38:49 +0100

    feat(criterion): Added adjacency criterion for the inspection of SurfaceMeshes

---
+++ src/geode/inspector/criterion/adjacency/surface_adjacency.cpp
@@ -0,0 +1,138 @@
+/*
+ * Copyright (c) 2019 - 2021 Geode-solutions
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#include <geode/inspector/criterion/adjacency/surface_adjacency.h>
+
+#include <geode/basic/pimpl_impl.h>
+
+#include <geode/mesh/core/surface_mesh.h>
+
+namespace geode
+{
+    template < index_t dimension >
+    class SurfaceMeshAdjacency< dimension >::Impl
+    {
+    public:
+        Impl( const SurfaceMesh< dimension >& mesh )
+        {
+            for( const auto polygon_id : Range{ mesh.nb_polygons() } )
+            {
+                for( const auto edge_id :
+                    LRange{ mesh.nb_polygon_edges( polygon_id ) } )
+                {
+                    const PolygonEdge polygon_edge{ polygon_id, edge_id };
+                    if( !mesh.is_edge_on_border( polygon_edge )
+                        && !mesh_polygon_edge_has_right_adjacency(
+                            mesh, polygon_edge ) )
+                    {
+                        polygons_wrong_adjacency_edges_.push_back(
+                            polygon_edge );
+                    }
+                }
+            }
+        }
+
+        bool mesh_has_wrong_adjacencies() const
+        {
+            return polygons_wrong_adjacency_edges_.size() > 0;
+        }
+
+        index_t nb_edges_with_wrong_adjacency() const
+        {
+            return polygons_wrong_adjacency_edges_.size();
+        }
+
+        const std::vector< PolygonEdge >&
+            polygon_edges_with_wrong_adjacency() const
+        {
+            return polygons_wrong_adjacency_edges_;
+        }
+
+    private:
+        bool mesh_polygon_edge_has_right_adjacency(
+            const SurfaceMesh< dimension >& mesh,
+            const PolygonEdge& polygon_edge )
+        {
+            const auto adjacent_polygon = mesh.polygon_adjacent( polygon_edge );
+            const auto polygon_adj_id = adjacent_polygon.value();
+            const auto v0 = mesh.polygon_vertex( polygon_edge );
+            const auto v1 = mesh.polygon_edge_vertex( polygon_edge, 1 );
+            for( const auto e :
+                LRange{ mesh.nb_polygon_edges( polygon_adj_id ) } )
+            {
+                const PolygonEdge adj_edge{ polygon_adj_id, e };
+                const auto adj_v0 = mesh.polygon_vertex( adj_edge );
+                if( adj_v0 == v1 )
+                {
+                    const auto adj_v1 = mesh.polygon_edge_vertex( adj_edge, 1 );
+                    if( adj_v1 == v0
+                        && mesh.polygon_adjacent( adj_edge )
+                               == polygon_edge.polygon_id )
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+    private:
+        std::vector< PolygonEdge > polygons_wrong_adjacency_edges_;
+    };
+
+    template < index_t dimension >
+    SurfaceMeshAdjacency< dimension >::SurfaceMeshAdjacency(
+        const SurfaceMesh< dimension >& mesh )
+        : impl_( mesh )
+    {
+    }
+
+    template < index_t dimension >
+    SurfaceMeshAdjacency< dimension >::~SurfaceMeshAdjacency()
+    {
+    }
+
+    template < index_t dimension >
+    bool SurfaceMeshAdjacency< dimension >::mesh_has_wrong_adjacencies() const
+    {
+        return impl_->mesh_has_wrong_adjacencies();
+    }
+
+    template < index_t dimension >
+    index_t
+        SurfaceMeshAdjacency< dimension >::nb_edges_with_wrong_adjacency() const
+    {
+        return impl_->nb_edges_with_wrong_adjacency();
+    }
+
+    template < index_t dimension >
+    const std::vector< PolygonEdge >&
+        SurfaceMeshAdjacency< dimension >::polygon_edges_with_wrong_adjacency()
+            const
+    {
+        return impl_->polygon_edges_with_wrong_adjacency();
+    }
+
+    template class opengeode_inspector_inspector_api SurfaceMeshAdjacency< 2 >;
+    template class opengeode_inspector_inspector_api SurfaceMeshAdjacency< 3 >;
+} // namespace geode
---
+++ tests/inspector/test-surface-adjacency.cpp
@@ -0,0 +1,308 @@
+/*
+ * Copyright (c) 2019 - 2021 Geode-solutions
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#include <geode/basic/assert.h>
+#include <geode/basic/logger.h>
+
+#include <geode/geometry/point.h>
+#include <geode/mesh/builder/triangulated_surface_builder.h>
+#include <geode/mesh/core/triangulated_surface.h>
+
+#include <geode/inspector/criterion/adjacency/surface_adjacency.h>
+
+void check_adjacency2D()
+{
+    auto surface = geode::TriangulatedSurface2D::create();
+    auto builder = geode::TriangulatedSurfaceBuilder2D::create( *surface );
+    builder->create_vertices( 4 );
+    builder->set_point( 0, { { 0., 1. } } );
+    builder->set_point( 1, { { 3., 0. } } );
+    builder->set_point( 2, { { 2., 4. } } );
+    builder->set_point( 3, { { 5., 3. } } );
+    builder->create_triangle( { 0, 1, 2 } );
+    builder->create_triangle( { 2, 1, 3 } );
+    builder->set_polygon_adjacent( { { 0, 1 } }, 1 );
+    builder->set_polygon_adjacent( { { 1, 0 } }, 0 );
+
+    const geode::SurfaceMeshAdjacency2D adjacency_inspector{ *surface };
+    OPENGEODE_EXCEPTION( !adjacency_inspector.mesh_has_wrong_adjacencies(),
+        "[Test] Surface has wrong adjacencies when it should have none." );
+    OPENGEODE_EXCEPTION(
+        adjacency_inspector.nb_edges_with_wrong_adjacency() == 0,
+        "[Test] Surface has wrong adjacencies on edges than it should." );
+    OPENGEODE_EXCEPTION(
+        adjacency_inspector.polygon_edges_with_wrong_adjacency().size() == 0,
+        "[Test] Surface edges adjacencies are shown wrong whereas they are "
+        "not." );
+}
+
+void check_non_adjacency_no_bijection2D()
+{
+    auto surface = geode::TriangulatedSurface2D::create();
+    auto builder = geode::TriangulatedSurfaceBuilder2D::create( *surface );
+    builder->create_vertices( 5 );
+    builder->set_point( 0, { { 0., 1. } } );
+    builder->set_point( 1, { { 3., 0. } } );
+    builder->set_point( 2, { { 2., 4. } } );
+    builder->set_point( 3, { { 5., 3. } } );
+    builder->set_point( 4, { { 1., 4. } } );
+    builder->create_triangle( { 0, 1, 2 } );
+    builder->create_triangle( { 2, 1, 3 } );
+    builder->create_triangle( { 4, 1, 2 } );
+    builder->set_polygon_adjacent( { { 0, 1 } }, 1 );
+    builder->set_polygon_adjacent( { { 1, 0 } }, 0 );
+    builder->set_polygon_adjacent( { { 2, 1 } }, 1 );
+
+    const geode::SurfaceMeshAdjacency2D adjacency_inspector{ *surface };
+    OPENGEODE_EXCEPTION( adjacency_inspector.mesh_has_wrong_adjacencies(),
+        "[Test] Surface should have a wrong adjacency due to non-bijection." );
+    OPENGEODE_EXCEPTION(
+        adjacency_inspector.nb_edges_with_wrong_adjacency() == 1,
+        "[Test] Surface should have one wrong adjacency due to "
+        "non-bijection." );
+    const geode::PolygonEdge polygon_edge{ { 2, 1 } };
+    OPENGEODE_EXCEPTION(
+        adjacency_inspector.polygon_edges_with_wrong_adjacency()[0]
+            == polygon_edge,
+        "[Test] Surface edges show wrong adjacency problems." );
+}
+
+void check_non_adjacency_wrong_edge2D()
+{
+    auto surface = geode::TriangulatedSurface2D::create();
+    auto builder = geode::TriangulatedSurfaceBuilder2D::create( *surface );
+    builder->create_vertices( 4 );
+    builder->set_point( 0, { { 0., 1. } } );
+    builder->set_point( 1, { { 3., 0. } } );
+    builder->set_point( 2, { { 2., 4. } } );
+    builder->set_point( 3, { { 5., 3. } } );
+    builder->create_triangle( { 0, 1, 2 } );
+    builder->create_triangle( { 2, 1, 3 } );
+    builder->set_polygon_adjacent( { { 0, 1 } }, 1 );
+    builder->set_polygon_adjacent( { { 1, 1 } }, 0 );
+
+    const geode::SurfaceMeshAdjacency2D adjacency_inspector{ *surface };
+    OPENGEODE_EXCEPTION( adjacency_inspector.mesh_has_wrong_adjacencies(),
+        "[Test] Surface should have wrong adjacencies due to wrong edge for "
+        "adjacency." );
+    OPENGEODE_EXCEPTION(
+        adjacency_inspector.nb_edges_with_wrong_adjacency() == 2,
+        "[Test] Surface should have two wrong adjacencies due to wrong edge "
+        "for adjacency." );
+    const geode::PolygonEdge polygon_edge1{ { 0, 1 } };
+    OPENGEODE_EXCEPTION(
+        adjacency_inspector.polygon_edges_with_wrong_adjacency()[0]
+            == polygon_edge1,
+        "[Test] Surface shows wrong first edge with adjacency problems." );
+    const geode::PolygonEdge polygon_edge2{ { 1, 1 } };
+    OPENGEODE_EXCEPTION(
+        adjacency_inspector.polygon_edges_with_wrong_adjacency()[1]
+            == polygon_edge2,
+        "[Test] Surface shows wrong second edge with adjacency problems." );
+}
+
+void check_non_adjacency_inversed_triangle2D()
+{
+    auto surface = geode::TriangulatedSurface2D::create();
+    auto builder = geode::TriangulatedSurfaceBuilder2D::create( *surface );
+    builder->create_vertices( 4 );
+    builder->set_point( 0, { { 0., 1. } } );
+    builder->set_point( 1, { { 3., 0. } } );
+    builder->set_point( 2, { { 2., 4. } } );
+    builder->set_point( 3, { { 5., 3. } } );
+    builder->create_triangle( { 0, 1, 2 } );
+    builder->create_triangle( { 1, 2, 3 } );
+    builder->set_polygon_adjacent( { { 0, 1 } }, 1 );
+    builder->set_polygon_adjacent( { { 1, 0 } }, 0 );
+
+    const geode::SurfaceMeshAdjacency2D adjacency_inspector{ *surface };
+    OPENGEODE_EXCEPTION( adjacency_inspector.mesh_has_wrong_adjacencies(),
+        "[Test] Surface should have wrong adjacencies due to an inversed "
+        "triangle." );
+    OPENGEODE_EXCEPTION(
+        adjacency_inspector.nb_edges_with_wrong_adjacency() == 2,
+        "[Test] Surface should have two wrong adjacencies due to an inversed "
+        "triangle." );
+    const geode::PolygonEdge polygon_edge1{ { 0, 1 } };
+    OPENGEODE_EXCEPTION(
+        adjacency_inspector.polygon_edges_with_wrong_adjacency()[0]
+            == polygon_edge1,
+        "[Test] Surface shows wrong first edge with adjacency problems due to "
+        "an inversed triangle.." );
+    const geode::PolygonEdge polygon_edge2{ { 1, 0 } };
+    OPENGEODE_EXCEPTION(
+        adjacency_inspector.polygon_edges_with_wrong_adjacency()[1]
+            == polygon_edge2,
+        "[Test] Surface shows wrong second edge with adjacency problems due to "
+        "an inversed triangle.." );
+}
+
+void check_adjacency3D()
+{
+    auto surface = geode::TriangulatedSurface3D::create();
+    auto builder = geode::TriangulatedSurfaceBuilder3D::create( *surface );
+    builder->create_vertices( 4 );
+    builder->set_point( 0, { { 0., 1., 0. } } );
+    builder->set_point( 1, { { 3., 0., 1. } } );
+    builder->set_point( 2, { { 2., 4., 2. } } );
+    builder->set_point( 3, { { 5., 3., 0. } } );
+    builder->create_triangle( { 0, 1, 2 } );
+    builder->create_triangle( { 2, 1, 3 } );
+    builder->set_polygon_adjacent( { { 0, 1 } }, 1 );
+    builder->set_polygon_adjacent( { { 1, 0 } }, 0 );
+
+    const geode::SurfaceMeshAdjacency3D adjacency_inspector{ *surface };
+    OPENGEODE_EXCEPTION( !adjacency_inspector.mesh_has_wrong_adjacencies(),
+        "[Test] 3D Surface has wrong adjacencies when it should have none." );
+    OPENGEODE_EXCEPTION(
+        adjacency_inspector.nb_edges_with_wrong_adjacency() == 0,
+        "[Test] 3D Surface has wrong adjacencies on edges than it should." );
+    OPENGEODE_EXCEPTION(
+        adjacency_inspector.polygon_edges_with_wrong_adjacency().size() == 0,
+        "[Test] 3D Surface edges adjacencies are shown wrong whereas they are "
+        "not." );
+}
+
+void check_non_adjacency_no_bijection3D()
+{
+    auto surface = geode::TriangulatedSurface3D::create();
+    auto builder = geode::TriangulatedSurfaceBuilder3D::create( *surface );
+    builder->create_vertices( 5 );
+    builder->set_point( 0, { { 0., 1., 0. } } );
+    builder->set_point( 1, { { 3., 0., 1. } } );
+    builder->set_point( 2, { { 2., 4., 2. } } );
+    builder->set_point( 3, { { 5., 3., 0. } } );
+    builder->set_point( 4, { { 1., 4., 4. } } );
+    builder->create_triangle( { 0, 1, 2 } );
+    builder->create_triangle( { 2, 1, 3 } );
+    builder->create_triangle( { 4, 1, 2 } );
+    builder->set_polygon_adjacent( { { 0, 1 } }, 1 );
+    builder->set_polygon_adjacent( { { 1, 0 } }, 0 );
+    builder->set_polygon_adjacent( { { 2, 1 } }, 1 );
+
+    const geode::SurfaceMeshAdjacency3D adjacency_inspector{ *surface };
+    OPENGEODE_EXCEPTION( adjacency_inspector.mesh_has_wrong_adjacencies(),
+        "[Test] 3D Surface should have a wrong adjacency due to "
+        "non-bijection." );
+    OPENGEODE_EXCEPTION(
+        adjacency_inspector.nb_edges_with_wrong_adjacency() == 1,
+        "[Test] 3D Surface should have one wrong adjacency due to "
+        "non-bijection." );
+    const geode::PolygonEdge polygon_edge{ { 2, 1 } };
+    OPENGEODE_EXCEPTION(
+        adjacency_inspector.polygon_edges_with_wrong_adjacency()[0]
+            == polygon_edge,
+        "[Test] 3D Surface edges show wrong adjacency problems." );
+}
+
+void check_non_adjacency_wrong_edge3D()
+{
+    auto surface = geode::TriangulatedSurface3D::create();
+    auto builder = geode::TriangulatedSurfaceBuilder3D::create( *surface );
+    builder->create_vertices( 4 );
+    builder->set_point( 0, { { 0., 1., 0. } } );
+    builder->set_point( 1, { { 3., 0., 1. } } );
+    builder->set_point( 2, { { 2., 4., 2. } } );
+    builder->set_point( 3, { { 5., 3., 0. } } );
+    builder->create_triangle( { 0, 1, 2 } );
+    builder->create_triangle( { 2, 1, 3 } );
+    builder->set_polygon_adjacent( { { 0, 1 } }, 1 );
+    builder->set_polygon_adjacent( { { 1, 1 } }, 0 );
+
+    const geode::SurfaceMeshAdjacency3D adjacency_inspector{ *surface };
+    OPENGEODE_EXCEPTION( adjacency_inspector.mesh_has_wrong_adjacencies(),
+        "[Test] 3D Surface should have wrong adjacencies due to wrong edge for "
+        "adjacency." );
+    OPENGEODE_EXCEPTION(
+        adjacency_inspector.nb_edges_with_wrong_adjacency() == 2,
+        "[Test] 3D Surface should have two wrong adjacencies due to wrong edge "
+        "for adjacency." );
+    const geode::PolygonEdge polygon_edge1{ { 0, 1 } };
+    OPENGEODE_EXCEPTION(
+        adjacency_inspector.polygon_edges_with_wrong_adjacency()[0]
+            == polygon_edge1,
+        "[Test] 3D Surface shows wrong first edge with adjacency problems." );
+    const geode::PolygonEdge polygon_edge2{ { 1, 1 } };
+    OPENGEODE_EXCEPTION(
+        adjacency_inspector.polygon_edges_with_wrong_adjacency()[1]
+            == polygon_edge2,
+        "[Test] Surface shows wrong second edge with adjacency problems." );
+}
+
+void check_non_adjacency_inversed_triangle3D()
+{
+    auto surface = geode::TriangulatedSurface3D::create();
+    auto builder = geode::TriangulatedSurfaceBuilder3D::create( *surface );
+    builder->create_vertices( 4 );
+    builder->set_point( 0, { { 0., 1., 0. } } );
+    builder->set_point( 1, { { 3., 0., 1. } } );
+    builder->set_point( 2, { { 2., 4., 2. } } );
+    builder->set_point( 3, { { 5., 3., 0. } } );
+    builder->create_triangle( { 0, 1, 2 } );
+    builder->create_triangle( { 1, 2, 3 } );
+    builder->set_polygon_adjacent( { { 0, 1 } }, 1 );
+    builder->set_polygon_adjacent( { { 1, 0 } }, 0 );
+
+    const geode::SurfaceMeshAdjacency3D adjacency_inspector{ *surface };
+    OPENGEODE_EXCEPTION( adjacency_inspector.mesh_has_wrong_adjacencies(),
+        "[Test] 3D Surface should have wrong adjacencies due to an inversed "
+        "triangle." );
+    OPENGEODE_EXCEPTION(
+        adjacency_inspector.nb_edges_with_wrong_adjacency() == 2,
+        "[Test] 3D Surface should have two wrong adjacencies due to an "
+        "inversed triangle." );
+    const geode::PolygonEdge polygon_edge1{ { 0, 1 } };
+    OPENGEODE_EXCEPTION(
+        adjacency_inspector.polygon_edges_with_wrong_adjacency()[0]
+            == polygon_edge1,
+        "[Test] 3D Surface shows wrong first edge with adjacency problems due "
+        "to an inversed triangle.." );
+    const geode::PolygonEdge polygon_edge2{ { 1, 0 } };
+    OPENGEODE_EXCEPTION(
+        adjacency_inspector.polygon_edges_with_wrong_adjacency()[1]
+            == polygon_edge2,
+        "[Test] 3D Surface shows wrong second edge with adjacency problems due "
+        "to an inversed triangle.." );
+}
+
+int main()
+{
+    try
+    {
+        check_adjacency2D();
+        check_non_adjacency_no_bijection2D();
+        check_non_adjacency_wrong_edge2D();
+        check_non_adjacency_inversed_triangle2D();
+        check_adjacency3D();
+        check_non_adjacency_no_bijection3D();
+        check_non_adjacency_wrong_edge3D();
+        check_non_adjacency_inversed_triangle3D();
+
+        geode::Logger::info( "TEST SUCCESS" );
+        return 0;
+    }
+    catch( ... )
+    {
+        return geode::geode_lippincott();
+    }
+}
