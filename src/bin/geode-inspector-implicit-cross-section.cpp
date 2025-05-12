6d983a3dfffb1172a837c7d47bad8601045798d5
MelchiorSchuh <melchior.schuh-senlis@geode-solutions.com>
2025-05-12 15:53:14 +0200

    fix(binaries): Added geode-inspector for implicit models.

---
+++ src/bin/geode-inspector-implicit-cross-section.cpp
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2019 - 2025 Geode-solutions
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#include <absl/flags/flag.h>
+#include <absl/flags/parse.h>
+#include <absl/flags/usage.h>
+
+#include <async++.h>
+
+#include <geode/basic/assert.hpp>
+#include <geode/basic/filename.hpp>
+#include <geode/basic/logger.hpp>
+
+#include <geode/geosciences/implicit/representation/core/implicit_cross_section.hpp>
+#include <geode/geosciences/implicit/representation/io/implicit_cross_section_input.hpp>
+
+#include <geode/geosciences_io/model/common.hpp>
+
+#include <geode/inspector/section_inspector.hpp>
+
+ABSL_FLAG( std::string, input, "/path/my/model.og_ixsctn", "Input model" );
+
+void inspect_implicit_cross_section(
+    const geode::ImplicitCrossSection& cross_section )
+{
+    const geode::SectionInspector cross_section_inspector{ cross_section };
+    auto result = cross_section_inspector.inspect_section();
+
+    geode::Logger::info( result.string() );
+}
+
+int main( int argc, char* argv[] )
+{
+    try
+    {
+        absl::SetProgramUsageMessage( absl::StrCat(
+            "ImplicitCrossSection inspector from Geode-solutions.\n",
+            "Sample usage:\n", argv[0], " --input my_cross_section.og_ixsctn\n",
+            "Default behavior tests all available criteria, to disable one "
+            "use --noXXX, e.g. --nocomponent_linking" ) );
+        absl::ParseCommandLine( argc, argv );
+
+        geode::GeosciencesIOModelLibrary::initialize();
+        const auto filename = absl::GetFlag( FLAGS_input );
+
+        inspect_implicit_cross_section(
+            geode::load_implicit_cross_section( filename ) );
+
+        return 0;
+    }
+    catch( ... )
+    {
+        return geode::geode_lippincott();
+    }
+}
