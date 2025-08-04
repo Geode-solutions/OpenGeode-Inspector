46bd83a029fcec6f1d3e378a6d178ee3f6932239
Pierre Anquez <pierre.anquez@geode-solutions.com>
2025-08-04 11:03:08 +0200

    test

--- tests/inspector/test-brep.cpp
+++ tests/inspector/test-brep.cpp
@@ -413,6 +413,20 @@ void check_wrong_bsurfaces_model()
     }
 }
 
+void check_meshed_cube()
+{
+    const auto model_brep = geode::load_brep(
+        absl::StrCat( geode::DATA_PATH, "meshed_cube.og_brep" ) );
+    const geode::BRepInspector brep_inspector{ model_brep };
+    const auto result = brep_inspector.inspect_brep();
+
+    geode::Logger::info( "meshed_cube topology is ",
+        brep_inspector.brep_topology_is_valid() ? "valid." : "invalid." );
+
+    const auto nb_component_meshes_issues =
+        launch_component_meshes_validity_checks( result.meshes, false );
+}
+
 void check_segmented_cube()
 {
     const auto model_brep = geode::load_brep(
@@ -433,12 +447,13 @@ int main()
     {
         geode::InspectorInspectorLibrary::initialize();
         geode::Logger::set_level( geode::Logger::LEVEL::debug );
-        check_model_a1( false );
-        check_model_a1_valid( false );
-        check_model_mss( false );
-        check_model_D( false );
-        check_wrong_bsurfaces_model();
-        check_segmented_cube();
+        // check_model_a1( false );
+        // check_model_a1_valid( false );
+        // check_model_mss( false );
+        // check_model_D( false );
+        // check_wrong_bsurfaces_model();
+        check_meshed_cube();
+        // check_segmented_cube();
         geode::Logger::info( "TEST SUCCESS" );
         return 0;
     }
